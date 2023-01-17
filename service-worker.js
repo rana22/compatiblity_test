fa5a45c3bea778021ffe2d0c210e5012c23bb09a
rana22 <ambar.rana123@gmail.com>
2023-01-17 13:42:04 -0500

    release

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7890206ab863613ecf3bc1887e01cd59.js"
+  "./precache-manifest.35909a4cde371ddaa14aa092ece7084b.js"
 );
 
 workbox.clientsClaim();
